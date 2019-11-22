e0a2ff0a303942622b0a437be02f7ad04774da2a
Jan Michalcak <alby@osboxes.org>
2019-11-22 20:30:07 +0100

    modified icons

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ $.get("localhost/v1/get_whatever", function (data) {
     $(".results").html(data);
     console.log("Load was performed.");
 });
+
+
+// Languages used
+
+// [{"team_name": "Prestiz", "table_number": 9, "repo_link": "https://github.com/Fenristan/Hackathon-2019", "language_amount": 0, "language_names": []}, {"team_name": "Gray Team", "table_number": 5, "repo_link": "https://github.com/blaza168/hack_2019", "language_amount": 1, "language_names": ["Python"]}, {"team_name": "Team20", "table_number": 20, "repo_link": "https://github.com/Kubos-cz/atthack20", "language_amount": 0, "language_names": []}, {"team_name": "Hundiska", "table_number": 10, "repo_link": "https://github.com/szymsza/att-hackathon", "language_amount": 0, "language_names": []}]
