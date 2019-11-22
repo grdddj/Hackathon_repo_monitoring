c37e818850f5a3effd7cdce5fbe8ddf160eabfd5
grdddj <jiri.musil06@seznam.cz>
2019-11-22 13:50:08 +0100

    Creating functions to do stuff

--- collecter.py
+++ collecter.py
@@ -1,62 +1,126 @@
 import requests
 import json
 
-token = "8b1d4f951340d361370167e2013a1ec986370a2e"
-headers = {"Authorization": "token {}".format(token)}
+# Information for Github API
+# NOTE: the first token suddently disappeared, and I do not know why
+TOKEN = "f868e0139417bf8ffc09341baa0376759c04587f"
+HEADERS = {"Authorization": "token {}".format(TOKEN)}
+
+# Aggregating all the repositories we want to track
+# Making it a set, not to accidentally put there one repo twice :)
+LIST_OF_REPOSITORIES = {
+    "https://github.com/Fenristan/Hackathon-2019",
+    "https://github.com/blaza168/hack_2019",
+    "https://github.com/Kubos-cz/atthack20",
+    "https://github.com/szymsza/att-hackathon"
+}
+
+# Templates for endpoints, to which user_name and repo_name should be inputted
+CONTRIBUTORS_ENDPOINT_TEMPLATE = "https://api.github.com/repos/{}/{}/contributors"
+LANGUAGES_ENDPOINT_TEMPLATE = "https://api.github.com/repos/{}/{}/languages"
+BRANCHES_ENDPOINT_TEMPLATE = "https://api.github.com/repos/{}/{}/branches"
+
+def _extract_name_and_repo_from_link(github_link):
+    """
+    Returns the username and repository name from the github repo link
+    """
+    split_link = github_link.split("/")
+    user_name = split_link[-2]
+    repo_name = split_link[-1]
+    print(user_name, repo_name)
+
+    return (user_name, repo_name)
 
 def get_rate_limit():
+    """
+    Helper function to see what limit we have
+    """
     rate_limit_endpoint = "https://api.github.com/rate_limit"
-    rate_limit_response = requests.get(rate_limit_endpoint, headers=headers).text
-    print(rate_limit_response)
+    rate_limit_response = requests.get(rate_limit_endpoint, headers=HEADERS).text
     rate_limit_dict = json.loads(rate_limit_response)
-    rate_limit = rate_limit_dict["resources"]["core"]["limit"]
-    print(rate_limit)
+    rate_limit = rate_limit_dict["resources"]["core"]["remaining"]
+    print("rate_limit", rate_limit)
 
-def get_contributors_and_amounts_of_commits():
-    rate_limit_endpoint = "https://api.github.com/repos/grdddj/Diploma-Thesis---Inverse-Heat-Transfer/contributors"
-    rate_limit_response = requests.get(rate_limit_endpoint, headers=headers).text
-    print(rate_limit_response)
-    rate_limit_dict = json.loads(rate_limit_response)
+def get_contributors_and_amounts_of_commits(user_name, repo_name):
+    """
+    Returning number of contributors and overall number of commits
+        for the whole repository
+    """
+    endpoint = CONTRIBUTORS_ENDPOINT_TEMPLATE.format(user_name, repo_name)
+    response = requests.get(endpoint, headers=HEADERS).text
+    response_dict = json.loads(response)
 
-    contributors_amount = len(rate_limit_dict)
-    print(contributors_amount)
+    contributors_amount = len(response_dict)
     commit_amounts = {}
-    for person in rate_limit_dict:
+    for person in response_dict:
         commit_amounts[person["login"]] = person["contributions"]
 
-    print(commit_amounts)
+    print("commit_amounts", commit_amounts)
+    print("contributors_amount", contributors_amount)
 
-def get_branches():
-    rate_limit_endpoint = "https://api.github.com/repos/grdddj/Diploma-Thesis---Inverse-Heat-Transfer/branches"
-    rate_limit_response = requests.get(rate_limit_endpoint, headers=headers).text
-    print(rate_limit_response)
-    rate_limit_dict = json.loads(rate_limit_response)
+    return {
+        "contributors_amount": contributors_amount,
+        "commit_amounts": commit_amounts
+    }
 
-    branch_amount = len(rate_limit_dict)
-    print(branch_amount)
+def get_branches(user_name, repo_name):
+    """
+    Returning number of branches for a certain repository,
+        together with their names
+    """
+    endpoint = BRANCHES_ENDPOINT_TEMPLATE.format(user_name, repo_name)
+    response = requests.get(endpoint, headers=HEADERS).text
+    response_dict = json.loads(response)
+
+    branch_amount = len(response_dict)
     branch_names = []
-    for branch in rate_limit_dict:
+    for branch in response_dict:
         branch_names.append(branch["name"])
-    print(branch_names)
 
-def get_languages():
-    rate_limit_endpoint = "https://api.github.com/repos/grdddj/Diploma-Thesis---Inverse-Heat-Transfer/languages"
-    rate_limit_response = requests.get(rate_limit_endpoint, headers=headers).text
-    print(rate_limit_response)
-    rate_limit_dict = json.loads(rate_limit_response)
+    print("branch_amount", branch_amount)
+    print("branch_names", branch_names)
 
-    language_amount = len(rate_limit_dict)
-    print(language_amount)
+    return {
+        "branch_amount": branch_amount,
+        "branch_names": branch_names
+    }
+
+
+def get_languages(user_name, repo_name):
+    """
+    Returning number of languages for a certain repository,
+        together with their names
+    """
+    endpoint = LANGUAGES_ENDPOINT_TEMPLATE.format(user_name, repo_name)
+    response = requests.get(endpoint, headers=HEADERS).text
+    response_dict = json.loads(response)
+
+    language_amount = len(response_dict)
     language_names = []
-    for language in rate_limit_dict:
+    for language in response_dict:
         language_names.append(language)
-    print(language_names)
+
+    print("language_amount", language_amount)
+    print("language_names", language_names)
+
+    return {
+        "language_amount": language_amount,
+        "language_names": language_names
+    }
 
 if __name__ == "__main__":
+    get_rate_limit()
+    for repo_link in LIST_OF_REPOSITORIES:
+        user_name, repo_name = _extract_name_and_repo_from_link(repo_link)
+
+        get_languages(user_name=user_name, repo_name=repo_name)
+        get_branches(user_name=user_name, repo_name=repo_name)
+        get_contributors_and_amounts_of_commits(user_name=user_name, repo_name=repo_name)
+
     # get_rate_limit()
-    # get_languages()
-    # get_branches()
-    get_contributors_and_amounts_of_commits()
+    # get_languages(user_name="szymsza", repo_name="att-hackathon")
+    # get_branches(user_name="szymsza", repo_name="att-hackathon")
+    # get_contributors_and_amounts_of_commits(user_name="szymsza", repo_name="att-hackathon")
 
 # rate_limit = rate_limit_dict["resources"]["core"]["limit"]
 # print(rate_limit)
